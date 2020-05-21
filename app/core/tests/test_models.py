25c1cca2d0c5f673878df1aad37c6eeef6259e08
Rish1711 <58459629+Rish1711@users.noreply.github.com>
2020-05-21 06:32:15 +0530

    Created TDD for user model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -8,8 +8,10 @@ class ModelTests(TestCase):
         """Test creating a new user with an email is successful"""
         email = 'rg171195@gmail.com'
         password = 'Password123'
-        user = get_user_model().objects.create_user(email=email,
-            password=password)
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+            )
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
 
@@ -27,8 +29,10 @@ class ModelTests(TestCase):
         """Test for creating super user"""
         email = 'rg171195@gmail.com'
         password = 'Password123'
-        user = get_user_model().objects.create_superuser(email=email,
-            password=password)
+        user = get_user_model().objects.create_superuser(
+            email=email,
+            password=password
+            )
 
         self.assertTrue(user.is_staff)
         self.assertTrue(user.is_superuser)
